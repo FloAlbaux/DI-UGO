73fe4eeadb894c5f62dccd4f916c73fe5ef9471b
florencealbaux <florence.albaux@ayrtonsystemes.com>
2023-11-11 17:49:56 +0100

    controller

--- backend/src/Controller/CustomerController.php
+++ backend/src/Controller/CustomerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,5 +17,62 @@ class CustomerController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/CustomerController.php',
         ]);
+    }
+
+    #[Route('/customers', name: 'app_customers')]
+    public function getCustomers(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $customers = $entityManager->getRepository(Customer::class)->findAll();
+
+        $data = [];
+        foreach ($customers as $customer) {
+            $data[] = [
+                'id' => $customer->getId(),
+                'title' => $customer->getTitle(),
+                'lastname' => $customer->getLastname(),
+                'firstname' => $customer->getFirstname(),
+                'postal_code' => $customer->getPostalcode(),
+                'city' => $customer->getCity(),
+                'email' => $customer->getEmail(), 
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+    #[Route('/customers/{customerId}/orders', name: 'app_customer_orders')]
+    public function getCustomerOrders(int $customerId, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $customer = $entityManager->getRepository(Customer::class)->find($customerId);
+
+        if (!$customer) {
+            return $this->json(['error' => 'Customer not found'], 404);
+        }
+        // $data[] = [
+        //     'id' => $customer->getId(),
+        //     'title' => $customer->getTitle(),
+        //     'lastname' => $customer->getLastname(),
+        //     'firstname' => $customer->getFirstname(),
+        //     'postal_code' => $customer->getPostalcode(),
+        //     'city' => $customer->getCity(),
+        //     'email' => $customer->getEmail(), 
+        // ];
+
+        $orders = $customer->getPurchases();
+
+        $data = [];
+        foreach ($orders as $order) {
+            $data[] = [
+                'id' => $order->getId(),
+                'purchaseIdentifier' => $order->getPurchaseIdentifier(),
+                'quantity' => $order->getQuantity(),
+                'price' => $order->getPrice(),
+                'currency' => $order->getCurrency(),
+                'date' => $order->getDate()->format('Y-m-d'), // Format de date selon vos besoins
+                // Ajoutez d'autres champs selon votre entité Order
+            ];
+        }
+
+        return $this->json($data);
     }
 }
